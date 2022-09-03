822bbcd81f2007e5c8b9ca47c52d453f4b4c3383
Faizan Sayyed <faizansayyed36@gmail.com>
2022-09-03 13:08:44 +0530

    added react-markdown

--- components/posts/post-details/post-content.js
+++ components/posts/post-details/post-content.js
@@ -1,5 +1,6 @@
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
+import ReactMarkdown from 'react-markdown';
 
 const DUMMY_POST = {
     slug: 'getting-started-with-nextjs',
@@ -16,6 +17,7 @@ function PostContent() {
         <article className={classes.content}>
             <PostHeader title={DUMMY_POST.title} image={imagePath} />
             {DUMMY_POST.content}
+            <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
         </article>
     );
 }
