043de8c6d39a54e5ea37f114f0a3f446fdaed368
Faizan Sayyed <faizansayyed36@gmail.com>
2022-09-04 15:58:41 +0530

    added env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = nextConfig
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: 'maximilian',
+        mongodb_password: '2YkcXq43KyPk0vqp',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'my-site-dev',
+      },
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: 'maximilian',
+      mongodb_password: '2YkcXq43KyPk0vqp',
+      mongodb_clustername: 'cluster0',
+      mongodb_database: 'my-site',
+    },
+  };
+};
