b3c1d16167ab12cd33d24672a8f72df41fef87a1
Faizan Sayyed <faizansayyed36@gmail.com>
2022-09-04 12:15:46 +0530

    added a contactform, getting + sending data on the client, storing data in mongodb, notification feedback for contact form, added head and metadata, added a portal

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,18 @@ import { Fragment } from 'react';
 import Hero from '../components/home-page/hero';
 import FeaturedPosts from '../components/home-page/featured-posts';
 import { getFeaturedPosts } from '../libs/posts-util';
+import Head from 'next/head';
 
 function HomePage(props) {
     return (
         <Fragment>
+            <Head>
+                <title>Faizan' Blog</title>
+                <meta
+                    name='description'
+                    content='I post about programming and web development.'
+                />
+            </Head>
             <Hero />
             <FeaturedPosts posts={props.posts} />
         </Fragment>
